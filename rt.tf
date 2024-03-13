e4b549c87843dc39b5b76837fea80237b1940f2e
EC2 Default User <ec2-user@ip-172-31-18-170.ec2.internal>
2024-03-13 12:50:59 +0000

    terraform file added

---
+++ rt.tf
@@ -0,0 +1,21 @@
+resource "aws_route_table" "myrt"{
+vpc_id = aws_vpc.vpc.id
+route{
+cidr_block = "0.0.0.0/0"
+gateway_id = "aws_internet_gateway.igw.id"
+}
+tags = {
+name = "myroutetable"
+}
+}
+
+resource "aws_route_table_association" "sn1"{
+subnet_id = aws_subnet.subnet1.id
+route_table_id = aws_route_table.myrt.id
+}
+
+resource "aws_route_table_association" "sn2"{
+subnet_id = aws_subnet.subnet2.id
+route_table_id = aws_route_table.myrt.id
+}
+
---
+++ vpc.tf
@@ -0,0 +1,8 @@
+resource "aws_vpc" "vpc" {
+tags = {
+Name = "myvpc1"
+}
+cidr_block = "10.0.0.0/16"
+instance_tenancy = "default"
+enable_dns_hostnames = "true"
+}
